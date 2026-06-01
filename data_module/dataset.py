c66f9a663a8d4b440d473632ba0d718bb4d93802
ReyChiaro <1402850762@qq.com>
2026-06-01 06:16:55 +0000

    update main before merge

--- data_module/dataset.py
+++ data_module/dataset.py
@@ -45,7 +45,7 @@ class SchemaDataset(Dataset):
     def __getitem__(self, index: int) -> dict[str, Any]:
         sample = self.samples[index % self.num_samples]
         prompt = sample["prompt"]
-        negative_prompt = sample.get("negative_prompt", None)
+        negative_prompt = sample.get("negative_prompt", "")
         conditions = sample["conditions"]
         target = sample["target"]
         image_name = Path(target).stem
