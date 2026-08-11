479108c45434344b8c10b6df65ece47a6936a054
ReyChiaro <1402850762@qq.com>
2026-08-11 16:44:04 +0000

    update poisson and scheduler

--- schedulers/mask_flow.py
+++ schedulers/mask_flow.py
@@ -112,12 +112,4 @@ class MaskFlowScheduler(RectifiedFlowMatchingScheduler):
     ):
         while sigma.ndim < xt.ndim:
             sigma = sigma.unsqueeze(-1)
-        # if source is None or mask is None or noise is None:
-        #     # return xt - sigma * v
-        #     return xt - (sigma / d_sigma_dt) * v
-        # if self.unmask_with in ["source", "target"]:
-        #     xt = mask * xt + (1 - mask) * source
-        # elif self.unmask_with in ["noisy_source", "noisy_target"]:
-        #     xt = mask * xt + (1 - mask) * ((1.0 - sigma) * source + sigma * noise)
-        # return xt - sigma * v
-        return xt - (sigma / d_sigma_dt) * v
+        return xt - sigma * v
